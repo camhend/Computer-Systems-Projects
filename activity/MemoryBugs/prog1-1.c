a4fefa3b1e35c47d14405d76fed31cacd6fc44b4
Cameron Henderson <henderc7@wwu.edu>
2024-02-06 15:45:09 -0800

    revert to original form

--- activity/MemoryBugs/prog1-1.c
+++ activity/MemoryBugs/prog1-1.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv){
   int i;
   int *a = malloc(sizeof(int) * 10);
   if (!a) return -1; 
-  for (i = 0; i < 11; i++){ // 10 bytes allocated - a[10] accesses an 11th integer 
+  for (i = 0; i < 11; i++) { // 10 bytes allocated - a[10] accesses an 11th integer 
     a[i] = i;
   }
   free(a);
