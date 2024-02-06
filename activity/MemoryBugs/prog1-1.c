14207f8f605f6f673dcea413b6ce0cd80dc490cc
Cameron Henderson <henderc7@wwu.edu>
2024-02-06 15:26:18 -0800

    begin tracking

---
+++ activity/MemoryBugs/prog1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+  int i;
+  int *a = malloc(sizeof(int) * 10);
+  if (!a) return -1; 
+  for (i = 0; i < 11; i++){ // 10 bytes allocated - a[10] accesses an 11th integer 
+    a[i] = i;
+  }
+  free(a);
+  return 0;
+}
